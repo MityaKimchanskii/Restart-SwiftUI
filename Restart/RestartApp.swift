d98619cffb79f576b803aa6e780ece3561f2068e
Dmitrii Kim <mitya_kim@icloud.com>
2022-10-09 22:50:57 -0700

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Mitya Kim on 10/9/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
